f916f40872f15e4efd6377c47bfa157e3f16e05a
Mischa Taylor <mischa@misheska.com>
2014-11-01 13:10:20 -0700

    Having a node['chef_server12']['nodes'] attribute is now optional

--- recipes/_add_nodes.rb
+++ recipes/_add_nodes.rb
@@ -3,14 +3,16 @@ validator_pem_path = \
 url = "https://#{node['chef_server12']['api_fqdn']}/organizations"\
       "/#{node['chef_server12']['organization']}"
 
-node['chef_server12']['nodes'].each do |_node_fqdn, node_ip|
-  # copy validator.pem to node
-  execute "scp -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no \
-    #{node['chef_server12']['organization_private_key_path']} \
-    root@#{node_ip}:#{validator_pem_path}"
+if node['chef_server12'].attribute?('nodes')
+  node['chef_server12']['nodes'].each do |_node_fqdn, node_ip|
+    # copy validator.pem to node
+    execute "scp -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no \
+      #{node['chef_server12']['organization_private_key_path']} \
+      root@#{node_ip}:#{validator_pem_path}"
 
-  # register the node and a client
-  execute "ssh -t -t -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no \
-    root@#{node_ip} 'chef-client \
-    --server #{url} --validation_key #{validator_pem_path}'"
+    # register the node and a client
+    execute "ssh -t -t -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no \
+      root@#{node_ip} 'chef-client \
+      --server #{url} --validation_key #{validator_pem_path}'"
+  end
 end
